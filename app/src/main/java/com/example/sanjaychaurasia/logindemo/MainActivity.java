41671ebb06072745dee9cb78813b0e2da3b807da
sanurasia <sanjay.csjm@gmail.com>
2018-12-08 16:53:27 +0530

    Added email and password validation.

--- app/src/main/java/com/example/sanjaychaurasia/logindemo/MainActivity.java
+++ app/src/main/java/com/example/sanjaychaurasia/logindemo/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.sanjaychaurasia.logindemo;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void signInAction(View v){
+        final TextInputLayout mEmailInput;
+        mEmailInput =  findViewById(R.id.textInputLayout);
+        String email = mEmailInput.getEditText().getText().toString();
+        if(emailValidator(email)){
+            Toast.makeText(this, "Valide Email", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(this, "Please enter valid email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final TextInputLayout passwordInput;
+        passwordInput = findViewById(R.id.passwordLayout);
+        String password = passwordInput.getEditText().getText().toString();
+        if(validatePassword(password)){
+            Toast.makeText(this, "Valide Password", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Password length must be greater or equal to 5 characters.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+    }
+
+    public boolean emailValidator(String email)
+    {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean validatePassword(String password) {
+        return password.length() > 5;
+    }
+
 }
